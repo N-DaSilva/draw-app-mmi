7d18ca441df0e2bb70456269a5a5b53074fd8d07
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2026-01-14 11:53:19 +0100

    add style

--- client/src/features/drawing/components/DrawToolbar/index.tsx
+++ client/src/features/drawing/components/DrawToolbar/index.tsx
@@ -1,10 +1,11 @@
 import { useColorStore } from "../../../../shared/store/useColorStore"
 import { useCallback } from "react"
+import styles from './DrawToolbar.module.css';
 
 export function DrawToolbar() {
   const setColor = useColorStore((state) => state.setColor)
 
-  const handleMouseClick = useCallback((e : React.MouseEvent<HTMLButtonElement>) => {
+  const handleMouseClick = useCallback((e : React.MouseEvent<HTMLInputElement>) => {
     if(e.currentTarget){
       console.log(e.currentTarget.value)
       setColor(e.currentTarget.value)
@@ -12,9 +13,15 @@ export function DrawToolbar() {
   }, [])
 
   return (
-    <div>
-      <button value={'black'} onClick={handleMouseClick}>Noir</button>
-      <button value={'red'} onClick={handleMouseClick}>Rouge</button>
+    <div className={styles.toolbar}>
+      <input type="radio" name="color" value='black' defaultChecked onClick={handleMouseClick} className="radio radio-lg " />
+      <input type="radio" name="color" value='red' onClick={handleMouseClick} className="radio radio-lg bg-red-100 border-red-300 checked:bg-red-200 checked:text-red-600 checked:border-red-600" />
+      <input type="radio" name="color" value='yellow' onClick={handleMouseClick} className="radio radio-lg bg-yellow-100 border-yellow-300 checked:bg-yellow-200 checked:text-yellow-600 checked:border-yellow-600" />
+      <input type="radio" name="color" value='green' onClick={handleMouseClick} className="radio radio-lg bg-green-100 border-green-300 checked:bg-green-200 checked:text-green-600 checked:border-green-600" />
+      <input type="radio" name="color" value='cyan' onClick={handleMouseClick} className="radio radio-lg bg-cyan-100 border-cyan-300 checked:bg-cyan-200 checked:text-cyan-600 checked:border-cyan-600" />
+      <input type="radio" name="color" value='blue' onClick={handleMouseClick} className="radio radio-lg bg-blue-100 border-blue-300 checked:bg-blue-200 checked:text-blue-600 checked:border-blue-600" />
+      <input type="radio" name="color" value='purple' onClick={handleMouseClick} className="radio radio-lg bg-purple-100 border-purple-300 checked:bg-purple-200 checked:text-purple-600 checked:border-purple-600" />
+      <input type="radio" name="color" value='#df007eff' onClick={handleMouseClick} className="radio radio-lg bg-pink-100 border-pink-300 checked:bg-pink-200 checked:text-pink-600 checked:border-pink-600" />
     </div>
   )
 }
